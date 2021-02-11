1d3a48be2f65446a1cc5b23b1051735f86b5e43d
vm-simarpreet <simarpreet.singh@venuemonk.com>
2021-02-11 22:59:15 +0530

    update + delete

--- app.js
+++ app.js
@@ -41,6 +41,29 @@ app.post("/addname", (req, res) => {
         });
 });
 
+app.post("/deletename/:id", (req, res) => {
+    User.remove({_id:req.params.id})
+        .then(item => {
+            res.redirect('/');
+        })
+        .catch(err => {
+            res.status(400).send("Unable to delete to database");
+        });
+});
+
+app.post("/updatename", (req, res) => {
+    let update={
+        firstName:req.body.firstName,
+        lastName:req.body.lastName,
+    }
+    User.update({_id:req.body.userid},update)
+        .then(item => {
+            res.redirect('/');
+        })
+        .catch(err => {
+            res.status(400).send("Unable to delete to database");
+        });
+});
 app.listen(port, () => {
     console.log("Server listening on port " + port);
 });
