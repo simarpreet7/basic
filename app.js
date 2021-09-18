7ab5c31568d44a8c1cff3efb08599127953f9ffc
simarpreet7 <samsingh7039.dtu@gmail.com>
2021-09-18 19:09:15 +0530

    deploy

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 var express = require("express");
 var app = express();
-var port = 3000;
+var port =  process.env.PORT ||3000;
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 var mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb+srv://sam:sam@cluster0.yf7kw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",{   useNewUrlParser: true,
+mongoose.connect(process.env.mongoUrl,{   useNewUrlParser: true,
 useUnifiedTopology: true,
 useCreateIndex: true,
 useFindAndModify: false });
